b0d4ebb750cd66b8a74b92ba87ae09c585a346bc
GambuzX <mg_mesquita@hotmail.com>
2020-07-11 15:18:23 +0100

    separated state into state and level manager

--- project/Assets/Scripts/Gameroom/GameState.cs
+++ project/Assets/Scripts/Gameroom/GameState.cs
@@ -3,68 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameState : MonoBehaviour
+public class GameState : Singleton<GameState> {
+    [SerializeField] private int money = 100;
+    [SerializeField] private List<string> transactionHistory = new List<string>();
 
-{
-    public static GameState gameState;
-
-    void Awake() {
-        if (!gameState) {
-            gameState = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else {
-            Destroy(gameObject);
-        }
+    public int getMoney() {
+        return money;
     }
 
-    private string[] minigames = {
-        "Evasion",
-        "Rythm",
-        "SlotMachine",
-        "TurnBased"
-    };
-
-    private string gameroomScene = "Gameroom";
-
-    private Dictionary<string, int> minigameFreq = new Dictionary<string,int>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        foreach (string minigame in minigames) {
-            minigameFreq.Add(minigame, 0);
-        }
-
-        StartCoroutine("stuff");
-    }
-
-    IEnumerator stuff() {
-        while(true) {
-            yield return new WaitForSeconds(2f);
-            loadRandomMinigame();
-        }
-    }
-
-    public void loadRandomMinigame() {
-        int minFreq = 999;
-        foreach (string minigame in minigames) {
-            minFreq = Mathf.Min(minFreq, minigameFreq[minigame]);
-        }
-
-        List<string> lessFrequentMinigames = new List<string>();
-        foreach (string minigame in minigames) {
-            if(minigameFreq[minigame] == minFreq)
-                lessFrequentMinigames.Add(minigame);
-        }
-
-        int choice = Random.Range(0, lessFrequentMinigames.Count);
-        string chosen = lessFrequentMinigames[choice];
-        minigameFreq[chosen]++;
-        SceneManager.LoadScene(chosen);
-    }
-
-    public void loadGameRoom() {
-        SceneManager.LoadScene(gameroomScene);
+    public void updateMoney(int change) {
+        money += change;
     }
 }
