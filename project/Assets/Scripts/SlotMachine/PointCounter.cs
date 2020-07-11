6415cfd71de960a69dc4b00586ab462a2e73a950
Guilherme Loureiro-Desktop <1171056@isep.ipp.pt>
2020-07-11 13:21:55 +0100

    Slot Machine Done

---
+++ project/Assets/Scripts/SlotMachine/PointCounter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PointCounter : MonoBehaviour
+{
+    public GameObject FirstDigit;
+    public GameObject SecondDigit;
+    public GameObject ThirdDigit;
+    public GameObject FourthDigit;
+    public GameObject FifthDigit;
+    public GameObject SixthDigit;
+    public GameObject PointsBox;
+    private int freq;
+    private int start = 0;
+    private static int NUM_DIGITS = 6;
+
+
+    // Update is called once per frame
+    public void countPoints()
+    {
+        StartCoroutine(Waiter());
+    }
+
+    private IEnumerator Waiter()
+    {
+        yield return new WaitForSecondsRealtime(5);
+
+        string finalString = FirstDigit.GetComponent<UnityEngine.UI.Text>().text +
+                             SecondDigit.GetComponent<UnityEngine.UI.Text>().text +
+                             ThirdDigit.GetComponent<UnityEngine.UI.Text>().text +
+                             FourthDigit.GetComponent<UnityEngine.UI.Text>().text +
+                             FifthDigit.GetComponent<UnityEngine.UI.Text>().text +
+                             SixthDigit.GetComponent<UnityEngine.UI.Text>().text;
+        
+        var characterCount= new Dictionary<char,int>();
+        
+        foreach(var c in finalString)
+        {
+            if(characterCount.ContainsKey(c))
+                characterCount[c]++;
+            else
+                characterCount[c] = 1;
+        }
+
+        int max = 1;
+        
+        foreach (var c in finalString)
+        {
+            if (characterCount[c]>max)
+            {
+                max = characterCount[c];
+            }
+        }
+        
+        PointsBox.GetComponent<UnityEngine.UI.Text>().text = "Points : " + max;
+
+    }
+}
---
+++ project/Assets/Scripts/SlotMachine/SlotSys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class SlotSys : MonoBehaviour
+{
+    private char[] numbersRand = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public Text slotNum;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        slotNum.text = numbersRand[Random.Range(0, numbersRand.Length)].ToString();
+    }
+
+    public void OffEnable()
+    {
+        Invoke("DelayNum", time);
+    }
+
+    public void OnEnable()
+    {
+        enabled = true;
+    }
+
+    void DelayNum()
+    {
+        enabled = false;
+    }
+}
