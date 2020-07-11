a677fcc1e94588743dd033d948a92255a9dbfc4e
GambuzX <mg_mesquita@hotmail.com>
2020-07-11 15:34:50 +0100

    coisas

--- project/Assets/Scripts/Evasion/EvasionTimer.cs
+++ project/Assets/Scripts/Evasion/EvasionTimer.cs
@@ -4,33 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EvasionTimer : Timer
-{
-    void Start() {
-        timeLeft = totalTime;
-    }
-
-    void Update() {
-        timeLeft -= Time.deltaTime;
-        timeLeft = Mathf.Max(timeLeft, 0);
-
-        updateUI();
-
-        if (timeLeft <= 0) {
-            WinGame();
-        }
-    }
-    
+{    
     public override void WinGame() {
         LevelManager levelManager = GameObject.FindObjectOfType<LevelManager>();
         GameState gameState = GameObject.FindObjectOfType<GameState>();
 
         levelManager.loadGameRoom();
-    }
-
-    private void updateUI() {
-        text.text = "" + Mathf.RoundToInt(timeLeft);
-        if(timeLeft < totalTime/5) {
-            text.color = Color.red;
-        }
     }
 }
